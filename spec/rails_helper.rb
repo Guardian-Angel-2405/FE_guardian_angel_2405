8e93edd94268bd9c12f6e9bba8632f0c043f4485
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-11 11:27:53 -0600

    Add VCR.configure block with temporary THROUGHLINE API KEY - this could change

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,3 +73,10 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<THROUGHLINE_API_KEY>') { Rails.application.credentials.throughline[:key] }
+  config.configure_rspec_metadata!
+end
