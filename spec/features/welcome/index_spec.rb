8c57afc0536036cd8a919118c41bfa0f0456a84c
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-11 12:14:57 -0600

    Add infrastructure to the test file

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Welcome Index", type: :feature do
+  describe "when I visit the landing page (root path)" do
+    it "displays the name of the app, a brief description, and the logo" do
+
+    end
+
+    it "displays the name of the app, a brief description, and the logo" do
+      
+    end
+  end
+end
