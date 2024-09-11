00eaa6055b05f153e62883a0052e5ce0ede1b8c6
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-11 13:53:40 -0600

    Add actions to the users and services controller

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,7 @@
+class ServicesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+end
