4d7b7f0bc8f378bbafe4ddbe09886f0fca31cdf6
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-11 12:59:47 -0600

    Add test for static features of the welcome page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 describe "Welcome Index", type: :feature do
   describe "when I visit the landing page (root path)" do
     it "displays the name of the app, a brief description, and the logo" do
+      visit root_path
+      expect(current_path).to eq(root_path)
 
+      expect(page).to have_content("Welcome to")
+      expect(page).to have_content("Guardian Angel")
+      expect(page).to have_content("Develop your mental wellness with the guidance of Guardian Angel")
     end
 
-    it "displays the name of the app, a brief description, and the logo" do
+    xit "displays the name of the app, a brief description, and the logo" do
       
     end
   end
