3f640a76dd61447f16d9c0ee536143b145ac432c
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-11 11:54:11 -0600

    Add uniqueness validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :email, :password, :phone_number
 
+  validates_uniqueness_of :email, :phone_number
+
   has_secure_password #by itself tells bcrypt its going to have password and password confirmation
 
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP #look into testing
