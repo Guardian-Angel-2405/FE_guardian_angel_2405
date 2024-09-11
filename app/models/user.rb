7768f0914dc7bdbcc619e81d2701b6b75afc547e
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-11 11:36:11 -0600

    Add validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  
+  validates_presence_of :first_name, :last_name, :email, :password, :phone_number
+
+  has_secure_password #by itself tells bcrypt its going to have password and password confirmation
+
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
