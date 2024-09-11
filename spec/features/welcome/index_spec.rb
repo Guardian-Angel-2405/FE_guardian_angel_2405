8a3e153581db9dc607667db8b95e48153e3ac3ef
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-11 13:26:11 -0600

    Add index spec tests for the buttons

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -11,8 +11,36 @@ describe "Welcome Index", type: :feature do
       expect(page).to have_content("Develop your mental wellness with the guidance of Guardian Angel")
     end
 
-    xit "displays the name of the app, a brief description, and the logo" do
-      
+    it "displays buttons for Log In, Sign Up, Emergency Services" do
+      visit root_path
+      expect(current_path).to eq(root_path)
+
+      expect(page).to have_content("Log In")
+      expect(page).to have_content("Sign Up")
+      expect(page).to have_content("Emergency Services")
+    end
+    
+    it "takes me to the Log In page when I click the Log In button" do
+      visit root_path
+      expect(current_path).to eq(root_path)
+
+      click_button "Log In"
+      expect(current_path).to eq(log_in_path)
+    end
+
+    it "takes me to the Sign Up page when I click the Sign Up button" do
+      visit root_path
+      expect(current_path).to eq(root_path)
+
+      click_button "Sign Up"
+      expect(current_path).to eq(new_user_path)
+    end
+    it "takes me to the Emergency Services page when I click the Emergency Services button" do
+      visit root_path
+      expect(current_path).to eq(root_path)
+
+      click_button "Emergency Services"
+      expect(current_path).to eq(services_path)
     end
   end
 end
