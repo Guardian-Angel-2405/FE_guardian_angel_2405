9861fc1df6d957cf7ef95b0d35657bb5ebce8a92
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-12 09:24:28 -0600

    Add create route for users with resource syntax

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   root "welcome#index"
 
-  resources :users, only: [:show, :index, :new] do
+  resources :users, only: [:show, :index, :new, :create] do
     resources :dashboard, only: [:index]
   end
   
