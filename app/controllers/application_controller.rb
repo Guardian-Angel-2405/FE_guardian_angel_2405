ecee42abed2cb3c9cbf051af7e1373c3f465cd05
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-12 10:45:42 -0600

    Add helper_method :current_user to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
   def current_user
-    User.find(session[:user_id])
+    @_current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
