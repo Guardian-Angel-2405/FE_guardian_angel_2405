27b21c33649267aac35247c5dacd23bb2b3e9cdc
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-12 10:49:47 -0600

    Fix incorrect variable name for user changing it from new user in the session lines for def login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   def login
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
-      session[:user_id] = new_user.id
+      session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.first_name}!"
       redirect_to user_dashboard_index_path(user.id)
     else
