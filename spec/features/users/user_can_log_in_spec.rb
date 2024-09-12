bb22a0cd6196d783d9f0cb4c59a9bbcb450a5f1e
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-12 15:49:06 -0600

    Add spec file for user_can_log_in_spec.rb

---
+++ spec/features/users/user_can_log_in_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "Logging In" do
+  it "can log in with valid credentials" do
+    user = User.create(email: "blackbeltslayer", first_name: "Nicky", password: "daslayer")
+
+    visit root_path
+
+    click_on "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in :username, with: user.email
+    fill_in :password, with: user.password
+
+    click_on "Log In"
+
+    expect(current_path).to eq(user_dashboard_index_path(user.id))
+
+    expect(page).to have_content("Welcome, #{user.first_name}")
+  end
+  it "cannot log in with bad credentials" do
+    user = User.create(email: "blackbeltslayer@thebteam.com", password: "daslayer")
+
+    visit login_path
+  
+    fill_in :username, with: user.username
+    fill_in :password, with: "incorrect password"
+  
+    click_on "Log In"
+  
+    expect(current_path).to eq(login_path)
+  
+    expect(page).to have_content("Sorry, your credentials are bad.")
+  end
+end
