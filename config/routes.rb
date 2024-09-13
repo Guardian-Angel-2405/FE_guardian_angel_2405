9c76cbf4cecd2bbe3dc02df23c057d9084c8854d
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-13 09:46:54 -0600

    Add routes for update and edit

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   root "welcome#index"
 
-  resources :users, only: [:show, :index, :new, :create] do
+  resources :users, only: [:show, :index, :new, :create, :update, :edit] do
     resources :dashboard, only: [:index]
   end
   
