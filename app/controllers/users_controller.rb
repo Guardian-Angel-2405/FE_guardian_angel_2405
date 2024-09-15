d596bae6b920638710b6514dc9b688b88a2900e2
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-15 10:45:26 -0600

    Fix error in login action within the users controller by adding user && infront of the user.authenticate(params[:password]) method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
 
   def login
     user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.first_name}!"
       redirect_to user_dashboard_index_path(user.id)
