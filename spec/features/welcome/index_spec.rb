5aa99c55a2a88b3fbf454e5eb17a387f39c77915
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-15 10:35:39 -0600

    Fix messages for it block on index_spec.rb

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -11,7 +11,7 @@ describe "Welcome Index", type: :feature do
       expect(page).to have_content("Develop your mental wellness with the guidance of Guardian Angel")
     end
 
-    it "displays buttons for Log In, Sign Up, Emergency Services" do
+    it "displays links for Log In, Sign Up, Emergency Services" do
       visit root_path
       expect(current_path).to eq(root_path)
 
@@ -20,7 +20,7 @@ describe "Welcome Index", type: :feature do
       expect(page).to have_content("Emergency Services")
     end
     
-    it "takes me to the Log In page when I click the Log In button" do
+    it "takes me to the Log In page when I click the Log In link" do
       visit root_path
       expect(current_path).to eq(root_path)
 
@@ -28,7 +28,7 @@ describe "Welcome Index", type: :feature do
       expect(current_path).to eq(login_path)
     end
 
-    it "takes me to the Sign Up page when I click the Sign Up button" do
+    it "takes me to the Sign Up page when I click the Sign In link" do
       visit root_path
       expect(current_path).to eq(root_path)
 
@@ -36,7 +36,7 @@ describe "Welcome Index", type: :feature do
       expect(current_path).to eq(new_user_path)
     end
 
-    it "takes me to the Emergency Services page when I click the Emergency Services button" do
+    it "takes me to the Emergency Services page when I click the Emergency Services link" do
       visit root_path
       expect(current_path).to eq(root_path)
 
