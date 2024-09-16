9e3437f5106f54fdb1943e2012073b60947e411d
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-16 15:04:52 -0600

    Remove tests for validating the presence of first and last name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 describe User, type: :model do
   describe "validations" do
-    it {should validate_presence_of(:first_name)}
-    it {should validate_presence_of(:last_name)}
     it {should validate_presence_of(:email)}
     it {should validate_presence_of(:password)}
     it {should validate_presence_of(:phone_number)}
