2c5a82619ffd8ca61435550eb77aea86b2e8063a
Dana Howell <howelld115@gmail.com>
2024-09-16 15:06:31 -0600

    wip, poros

--- app/controllers/emergency_services_controller.rb
+++ app/controllers/emergency_services_controller.rb
@@ -2,7 +2,8 @@ class EmergencyServicesController < ApplicationController
   def index
     country_code = params[:country_code] || 'us'
     @helplines = HelplineService.get_helplines(country_code)
-
+    # refactor this:
+    helpline_poros(@helplines)
     if @helplines.is_a?(Hash) && @helplines[:error]
       flash[:error] = @helplines[:error]
       @helplines = []
@@ -11,7 +12,7 @@ class EmergencyServicesController < ApplicationController
 
   def show
     @helpline = HelplineService.get_helpline_details(params[:id])
-
+    helpline_poros(@helpline)
     if @helpline[:error]
       flash[:error] = @helpline[:error]
       redirect_to emergency_services_path
