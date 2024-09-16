3c4f9a029bdc0c9734db17423e7d53990c48605e
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-16 14:44:29 -0600

    Changes to the views for login_form.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   def omniauth
     user = User.from_omniauth(request.env["omniauth.auth"])
     
+    binding.pry
     if user.valid?
       session[:user.id] = user.id
       redirect_to user_dashboard_index_path(user)
