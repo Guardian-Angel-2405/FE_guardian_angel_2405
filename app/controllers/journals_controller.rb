f58b9840e1cde2d6fd77b523c76e6c9b84e126cf
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-18 15:12:58 -0600

    Add index action with journal instance variable for the journals_controller

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,6 +1,20 @@
 class JournalsController < ApplicationController
   def index 
     @user = User.find(params[:user_id])
-    
+
+    @journal = JournalService.get_dates(@user.id)
+
+    binding.pry
+    # refactor this:
+    # helpline_poros(@helplines)
+    # if @helplines.is_a?(Hash) && @helplines[:error]
+    #   flash[:error] = @helplines[:error]
+    #   @helplines = []
+    # end
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @entry = Journal.find(params[:id])
   end
 end
