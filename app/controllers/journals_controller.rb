aad7d9767540fc6ee6b374dfeacb0f6aab297713
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-19 11:56:39 -0600

    Add new infrastrucutre for creating a new entry and controller actions for doing so

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -8,11 +8,30 @@ class JournalsController < ApplicationController
     #   flash[:error] = @helplines[:error]
     #   @helplines = []
     # end
+    binding.pry
   end
 
   def show
     @user = User.find(params[:user_id])
     @date= params[:id]
     @journal = JournalService.get_gratitude(@user.id, @date_id)
+  end
+
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)  
+    
+    if @user.save 
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.first_name}!"
+      redirect_to user_dashboard_index_path(@user.id)
+    elsif !@user.save
+      flash[:error] = "There was a problem creating your account. Please check the form for errors."
+      render :new  
+    end
   end
 end
